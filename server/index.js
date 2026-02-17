9bf9fd0a0c692efdedde36f56cf2b763483015cf
Mikhail Ivanovskii <stathmaj@gmail.com>
2026-02-17 17:27:43 +1000

    update

--- server/index.js
+++ server/index.js
@@ -1,35 +1,12 @@
 const express = require("express");
-const https = require('https');
 const mail = require('./mail.js')
-const fs = require('fs')
 const app = express();
-
+const port = 3000;
 var cors = require('cors')
 
 const jsonParser = express.json();
 
-const privateKey = fs.readFileSync('localhost-key.pem', 'utf8');
-const certificate = fs.readFileSync('localhost.pem', 'utf8');
-const passphrase = 'gaurav'; // Replace with your passphrase
-const credentials = { key: privateKey, passphrase, cert: certificate };
-
-// Create an HTTPS server with your Express app
-const httpsServer = https.createServer(credentials, app);
-
 app.use(cors())
-
-// Define a middleware to redirect HTTP to HTTPS
-// function ensureSecure(req, res, next) {
-//     if (req.secure) {
-//         // Request is already secure (HTTPS)
-//         return next();
-//     }
-//     // Redirect to HTTPS version of the URL
-//     res.redirect('https://' + req.hostname + req.originalUrl);
-// }
-
-// Use the middleware to enforce HTTPS
-// app.use(ensureSecure);
 
 app.post("/api/sendForm", jsonParser, function (req, res) {
   if (!req.body) return res.sendStatus(400);
@@ -37,4 +14,4 @@ app.post("/api/sendForm", jsonParser, function (req, res) {
   mail(req.body)
 });
 
-app.listen(3000, () => console.log(`Example app listening!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
